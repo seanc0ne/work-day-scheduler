91d5d4db05b35adcc5e33ee06cfed94453dd5333
Sean Cone <seandoesitbetter@gmail.com>
2020-06-27 08:52:10 -0700

    Fixed localStorage and added cursor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Array of times using military time as integers
 var times = [
     9,
     10,
@@ -12,19 +13,19 @@ var times = [
     19
 ];
 
+// Bind to the existing container div
 var container = $('.container');
 var row, col;
 
+// Loop through the times, create rows with specific columns
 for (var i = 0; i < times.length; i++) {
     let rowTime = moment({hour: times[i]});
     let currentHourTime = (moment().startOf('hour'));
-    console.log(rowTime.format('h:mm a'));
-
-    console.log(rowTime.isBefore(moment()));
 
     row = $('<div></div>').addClass('row');
     row.addClass('time-slot');
 
+    // Color-coding the rows via classes
     if (rowTime.isBefore(currentHourTime)){
         row.addClass('past');
     } else if(rowTime.isAfter(currentHourTime)) {
@@ -33,43 +34,64 @@ for (var i = 0; i < times.length; i++) {
         row.addClass('present');
     }
 
+    // First column
     col = $('<div></div>').addClass('col');
     col.addClass('hour');
     col.html(rowTime.format("h:mm a"));
 
     row.append(col);
 
-    col = $('<div></div>').addClass('col-10').attr("id", times[i].toString());
+    // Second column
+    col = $('<textarea></textarea>').addClass('col-10').attr("id", times[i].toString());
     col.html('Put data here');
 
     row.append(col);
 
+    // Third column creating the <div class="saveBtn"></div>
     col = $('<div></div>').addClass('col');
-    col.addClass('saveBtn').click(function() {});
+    col.addClass('saveBtn');
+
     col.attr("title", times[i].toString());
-    col.html('Edit');
+    col.html('Save');
 
     row.append(col);
 
+    // Append the row to the container
     container.append(row);
 }
 
+
+// Show current date at the top of the page
 var currentDate = new Date();
 var rightNow = moment().format('dddd, MMMM Do');
-
 $('#currentDay').html(rightNow);
 
-$('.time-slot').on('click', () => {
-
-    console.log("Clicked this", this)
 
 
-});
 
+// Click the button and save to local storage
+// Binding click handler to the <div class="saveBtn"></div>
 $('.saveBtn').on('click', function() {
     var id = $(this).attr('title');
     
     localStorage.setItem(id, $("#" + id).val());
 });
 
-$("#" + id).val(localStorage.getItem(id));
+
+
+
+for (var i = 0; i < times.length; i++) {
+  var id = times[i].toString();
+
+  var savedText = localStorage.getItem(id);
+
+  if (!savedText) {
+    var textToDisplay = "Put data here";
+  }
+  else {
+    var textToDisplay = savedText;
+  }
+
+  // Pull from local storage to display the value in each row.
+  $("#" + id).html(textToDisplay);
+}
